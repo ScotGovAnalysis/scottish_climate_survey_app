47de07440d852c1b59208a035472761d3812333a
irisoren-sg <iris.oren@gov.scot>
2026-02-12 14:49:29 +0000

    Comment and tidy tidymodels_test_lasso_smote_cluster.R

--- inst/tidymodels_test_lasso_smote_cluster.R
+++ inst/tidymodels_test_lasso_smote_cluster.R
@@ -1,3 +1,8 @@
+# This script uses SMOTE on the qclimate1 response variable, collapsed into a binary outcome
+# Followed by hierarchical clustering of predictors to see which variables are in
+# clusters with the selected predictors
+
+
 library(tidymodels)
 #library(vip)
 library(shiny)
@@ -72,90 +77,7 @@ dat <- read_workbook(path, 6 * 1024^2)
 data_tbl <- dat$data
 varlab   <- dat$varlab
 vallab   <-dat$vallab
-# ###############
-# data_tbl_processed <- data_tbl %>% 
-#   select(-`@weight0`, -`@weight1`, 
-#          -(starts_with("qc1") & !matches("qc1_1_actionscale_all"))) %>% 
-#   mutate(across(everything()), replace(., . == -99, NA)) %>% 
-#   filter(!is.na(qc1_1_actionscale_all)) %>% 
-#   mutate(response = case_when(
-#     qc1_1_actionscale_all %in% as.character(4:7) ~ 4, 
-#     TRUE ~ qc1_1_actionscale_all
-#   )) %>% 
-#   mutate_all(as.factor) %>% 
-#   select(-qc1_1_actionscale_all)
-# 
-# split <- initial_split(data_tbl_processed, strata = response)
-# 
-# train <- training(split)
-# test <- testing(split)
-# 
-# 
-# rec <- recipe(response ~ ., data = train) %>%
-#   # 1) Impute missing categories
-#   step_impute_mode(all_nominal_predictors()) %>%
-#   
-#   # 2) Remove single-level predictors *before* dummying
-#   step_zv(all_nominal_predictors()) %>%
-#   
-#   # 3) Collapse rare levels to stabilize across folds (tune threshold if needed)
-#   step_other(all_nominal_predictors(), threshold = 0.005) %>%
-#   
-#   # 4) One-hot / dummy encoding (reference encoding by default is fine for glmnet)
-#   step_dummy(all_nominal_predictors()) %>%
-#   
-#   # 5) Clean up any constant columns created after encoding
-#   step_zv(all_predictors())
-# 
-# 
-# 
-# 
-# 
-# mod_lasso <-  parsnip::multinom_reg(
-#   penalty = tune::tune(),
-#   mixture = 1
-# ) %>% parsnip::set_engine("glmnet")
-# 
-# param_set_lasso <- parameters(dials::penalty())
-# 
-# lasso_grid <- dials::grid_regular(param_set_lasso)
-# 
-# 
-# folds <- rsample::vfold_cv(v = 5,
-#                            strata = response, 
-#                            data = train)
-# 
-# wf <- 
-#   workflow() %>%
-#   add_model(mod_lasso) %>%
-#   add_recipe(rec)
-# 
-# ctrl <- control_grid(save_workflow = TRUE)
-# 
-# set.seed(456)
-# tune_lasso <- tune_grid(wf, resamples = folds, grid = lasso_grid, 
-#                      control = ctrl)
-# fit_lasso <- fit_best(tune_lasso)
-# 
-# prd <- predict(fit_lasso, test)
-# #vip::vi(fit_rf)
-# 
-# 
-# precision_vec(test$response, prd$.pred_class)
-# recall_vec(test$response, prd$.pred_class)
-# accuracy_vec(test$response, prd$.pred_class)
-# bal_accuracy_vec(test$response, prd$.pred_class)
 
-#> precision_vec(test$response, prd$.pred_class)
-# [1] 0.3298142
-# > recall_vec(test$response, prd$.pred_class)
-# [1] 0.308258
-# > accuracy_vec(test$response, prd$.pred_class)
-# [1] 0.5982405
-# > bal_accuracy_vec(test$response, prd$.pred_class)
-# [1] 0.5574976
-#########################
-#########################data_tbl_processed <- data_tbl %>% 
 data_tbl_processed <- data_tbl %>% 
   select(-`@weight0`, -`@weight1`       ) %>% 
   mutate(across(everything()), replace(., . == -99, NA)) %>% 
@@ -186,25 +108,24 @@ rec_class2 <- recipe(response_binary ~ ., data = train) %>%
   step_smote(response_binary) %>%  # SMOTE works much better for binary
   step_zv(all_predictors())
 
-# rec_upsample <-  recipe(response ~ ., data = train) %>%
-#   step_impute_mode(all_nominal_predictors()) %>%
-#   step_other(all_nominal_predictors(), threshold = 0.005) %>%
-#   step_dummy(all_nominal_predictors()) %>%
-#   step_upsample(response, over_ratio = 1) %>%
-#   step_zv(all_predictors())
-    
+
 
 mod_glm <-  parsnip::multinom_reg(
   penalty = tune::tune(),
   mixture = tune::tune()
 ) %>% parsnip::set_engine("glmnet")
 
+
+# Use a two step cross-validation 
+# First phase use a course grid for penalty and mixture
+# Select best mixture
+# Then tune penalty at best mixture
 param1 <- parameters(dials::penalty(range = c(-5, -2)), 
                               dials::mixture())
 
 
 grid1 <- grid_regular(param1, levels = c(5, 5))
-# lasso_grid <-  dials::grid_latin_hypercube(param_set_lasso, size = 50)
+
 
 
 folds <- rsample::vfold_cv(v = 5,
@@ -218,8 +139,6 @@ wf <-
 
 ctrl <- control_grid(save_workflow = TRUE)
 
-
-
 metrics <- yardstick::metric_set(accuracy, mn_log_loss, kap)
 
 set.seed(456)
@@ -229,7 +148,7 @@ tune_glm <- tune_grid(wf, resamples = folds, grid = grid1,
 best_ll <- tune::select_best(tune_glm, metric = "mn_log_loss")
 best_mixture <- best_ll$mixture
 
-
+# Now tune penalty at best mixture
 mod2 <- multinom_reg(penalty = tune(), mixture = best_mixture) %>%
   set_engine("glmnet")
 param2 <- parameters(dials::penalty(range = c(-8, -1)))
@@ -253,9 +172,7 @@ tune_glm2 <- tune_grid(
 
 best_ll2 <- tune::select_best(tune_glm2, metric = "mn_log_loss")
 
-# tune_lasso <- tune_grid(wf, resamples = folds, grid = lasso_grid, 
-#                         control = ctrl)
-
+# Finalise, predict and evaluate
 wf_final <- finalize_workflow(wf2, best_ll2)
 fit_glm <- fit(wf_final, data = train)
 
